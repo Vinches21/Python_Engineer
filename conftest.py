3aa255083418452a55fdd5a7990b9c34851c885b
fantomas <Vinches2193@yandex.ru>
2025-04-29 00:43:38 +0300

    refactoring

--- conftest.py
+++ conftest.py
@@ -6,14 +6,16 @@ import pytest
 from PIL import Image
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 
 @pytest.fixture(scope="function")
 def driver():
     options = Options()
     options.add_argument("headless")
     options.add_argument("no-sandbox")
-    driver = webdriver.Chrome(options=options)
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=options)
     yield driver
 
 
--- test_allure.py
+++ test_allure.py
@@ -4,28 +4,28 @@ import pytest
 from UI_Selenium.pages.base_class import BaseClass
 
 
-# @allure.title("Проверка равенства")
-# @allure.description("Этот тест проверяет, что x > y")
-# def test_positive_one():
-#     x = 2
-#     y = 3
-#     assert x < y, "Не вверно"
-#
-# @allure.title("Проверка сложения")
-# @allure.description("Этот тест проверяет сумму")
-# def test_positive_two():
-#     x = 2
-#     y = 3
-#     res = x + y
-#     assert res == 5, "Не вверно"
-#
-#
-# @allure.title("Специально созданный негативный тест")
-# @allure.description("Этот тест проверяет что тест проваливается")
-# def test_negative():
-#     x = 2
-#     y = 3
-#     assert x > y, "Не вверно"
+@allure.title("Проверка равенства")
+@allure.description("Этот тест проверяет, что x > y")
+def test_positive_one():
+    x = 2
+    y = 3
+    assert x < y, "Не вверно"
+
+@allure.title("Проверка сложения")
+@allure.description("Этот тест проверяет сумму")
+def test_positive_two():
+    x = 2
+    y = 3
+    res = x + y
+    assert res == 5, "Не вверно"
+
+
+@allure.title("Специально созданный негативный тест")
+@allure.description("Этот тест проверяет что тест проваливается")
+def test_negative():
+    x = 2
+    y = 3
+    assert x > y, "Не вверно"
 
 @pytest.mark.open
 def test_open_yandex(driver):
